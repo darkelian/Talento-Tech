1574d8d80920f240c630047afef4863e3d257302
eadiazm <eadiazm@gmail.com>
2024-06-30 11:38:32 -0500

    Modificaciones en controllers, consulta por estatus, cambio estatus en reservation, y consulta por tutor y estatus

--- app/src/routes/reservationRoutes.js
+++ app/src/routes/reservationRoutes.js
@@ -6,23 +6,23 @@ const router = express.Router();
 
 const { newReservation,
      getReservationById,
-     getAllReservations, 
-     updateReservation, 
+     getAllReservations,
+     updateReservation,
      deleteReservation,
-     getReservationsByTutorId,
+     getReservationsByTutorIdAndStatus,
      getReservationsByStudentId } = require("../controllers/reservationController")
 
 //(CREATE) Ruta para crear un nuevo reservation 
 router.route('/reservation/new').post(newReservation);
 
 //(FIND) Ruta consulta reservation x id
-router.route('/reservation/:id').get(getReservationById);  
+router.route('/reservation/:id').get(getReservationById);
 
 //(FIND) Ruta consulta reservation x student id
-router.route('/reservations/student/:id').get(getReservationsByStudentId); 
+router.route('/reservations/student/:id').get(getReservationsByStudentId);
 
 //(FIND) Ruta consulta reservation x tutor id
-router.route('/reservations/tutor/:id').get(getReservationsByTutorId);  
+router.route('/reservations/tutor/:id/:status').get(getReservationsByTutorIdAndStatus);
 
 //Ruta para consultar todos los reservations
 router.route('/reservations').get(getAllReservations);
@@ -31,7 +31,7 @@ router.route('/reservations').get(getAllReservations);
 router.route('/reservation/:id').put(updateReservation);
 
 //(DELETE) Ruta eliminar reservation x id 
-router.route('/reservation/:id').delete(deleteReservation); 
+router.route('/reservation/:id').delete(deleteReservation);
 
 //Exportamos el router
 module.exports = router;
